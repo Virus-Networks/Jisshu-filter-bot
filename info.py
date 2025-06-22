97f02f7df8d50b14ad96d4a3cada929d086229f0
Virus-Networks <168676321+Virus-Networks@users.noreply.github.com>
2025-06-22 16:29:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,11 +35,11 @@ LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1001995831309'))
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1001995831309'))
 
 # MongoDB 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://V2Premium:V2Premium@v2premium.ryila.mongodb.net/?retryWrites=true&w=majority&appName=V2Premium")
-DATABASE_NAME = environ.get('DATABASE_NAME', "V2Premium")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://FileStore:FileStore@cluster0.ant6zrf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 
 # Files index database url
-FILES_DATABASE = environ.get('FILES_DATABASE', "mongodb+srv://V2Premium:V2Premium@v2premium.ryila.mongodb.net/?retryWrites=true&w=majority&appName=V2Premium")
+FILES_DATABASE = environ.get('FILES_DATABASE', "mongodb+srv://FileStore:FileStore@cluster0.ant6zrf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Wamika')
 
 # Other Channel's
